14f3e06f9393c44f4dd655f569e8fd08c4b701e7
Eldryr62570 <jordan.moulin62570@gmail.com>
2023-02-16 15:58:55 +0100

    Server websocket ready

--- arosaje/back-chat/index.js
+++ arosaje/back-chat/index.js
@@ -3,19 +3,26 @@ const app = express();
 const http = require("http");
 const { Server } = require('socket.io')
 const cors = require("cors")
-app.use(cors());
 
+
+app.use(cors());
 const server = http.createServer(app)
-const io = new Server(server , {
-    cors:{
-        origin: "http://localhost:3000",
-        methods : ["GET" , "POST"]
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true
     }
-})
+});
+    
 io.on("connection" , (socket)=>{
-    socket.on("send_message"  , (data) =>{
-        socket.broadcast.emit("receive_message" , data)
+    socket.on("send_message"  , ({idUser,idRoom,message,time}) =>{
+        socket.to(idRoom).emit("receive_message" ,{idUser,idRoom,message,time})
     })
+    socket.on("join_room" , ({idUser, idRoom}) => {
+      socket.join(idRoom)
+    })
+
 })
 server.listen(3001, () => {
     console.log("SERVER IS RUNNING");
